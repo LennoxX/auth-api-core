6f0b316cdc4e7e7e9f3101715ff065c53eb50c90
Lucas Raphael Fernandes Ferreira <lucasraphael.fernandes@hotmail.com>
2020-11-16 14:55:47 -0300

    First Commit

---
+++ AuthApi/Controllers/AuthController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using AuthApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+
+namespace AuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+        public AuthController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+
+        [Route("signin")]
+        [HttpPost]
+        public IActionResult Login([FromBody] User loginDetails)
+        {
+            bool resultado = ValidarUsuario(loginDetails);
+            if (resultado)
+            {
+                var tokenString = GerarTokenJwt();
+                return Ok(new { token = tokenString });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+        private bool ValidarUsuario(User loginDetails)
+        {
+            if (loginDetails.UserName == "mac" && loginDetails.Password == "numsey")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string GerarTokenJwt()
+        {
+            var issuer = _config["Jwt:Issuer"];
+            var audience = _config["Jwt:Audience"];
+            var expiry = DateTime.Now.AddMinutes(60);
+            var securityKey = new SymmetricSecurityKey
+                              (Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials
+                              (securityKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(issuer: issuer,
+                                             audience: audience,
+                                             expires: DateTime.Now.AddMinutes(120),
+                                             signingCredentials: credentials);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var stringToken = tokenHandler.WriteToken(token);
+            return stringToken;
+        }
+
+        [Authorize]
+        [Route("validate")]
+        [HttpPost]
+        public ActionResult<string> Validate()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ AuthApi/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Maria", "Paulo", "Pedro", "Marcia", "Armando" };
+        }
+        [Authorize]
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "Marta";
+        }
+}
+}
